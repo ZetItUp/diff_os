4d60f1c5456bab696d5028bd9ce39cfaeecae9b7
Andreas <zetitup@gmail.com>
2025-07-23 15:16:29 +0200

    More Booting

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,70 +2,66 @@
 [ORG 0x7C00]
 
 entry:
-    cli
+    cli                                 ; Stop interrupts on the CPU
     xor ax, ax
     mov ds, ax
     mov es, ax
-    mov ss, ax
+    mov ss, ax                          ; Clear all segments
 
     mov sp, 0x8000
-    mov bp, sp
+    mov bp, sp                          ; Set stack pointer and base pointer
     
-    ; Save boot drive and disable SMM
-    mov [boot_drive], dl
-    ;call disable_smm
-    ; Enable A20
-    call enable_a20
+    mov [boot_drive], dl                ; Store the boot drive that BIOS gave us
+    
+    call enable_a20                     ; Enable the A20 line
     mov ax, 0
     mov ds, ax
     mov es, ax
-    jnc .a20ok
-    mov si, msg_a20_fail
+    jnc .a20ok                          ; If we succeed, continue with loading the kernel
+    mov si, msg_a20_fail                ; If we fail, crash and halt CPU
     jmp error
 .a20ok:
 
-    mov cx, 3
-.load_kernel:
-    mov byte [bp], 0x10           ; Size of DAP
+    mov cx, 3                           ; Attempt to read the kernel 3 times
+.load_kernel:   
+    mov byte [bp], 0x10                 ; Size of DAP
     mov byte [bp+1], 0x00
 
-%include "build/kernel_sizes.inc"
-    mov word [bp+2], KERNEL_SECTORS            ; kernel sectors
-    mov word [bp+4], 0x0000       ; Offset
-    mov word [bp+6], 0x1000        ; Segment (0x7E0:0 = 0x7E00)
-    mov dword [bp+8], 2048        ; LBA
+%include "build/kernel_sizes.inc"       ; Include kernel_sizes.inc to get calculated sizes from buildtime
+    mov word [bp+2], KERNEL_SECTORS     ; Kernel sectors
+    mov word [bp+4], 0x0000             ; Offset
+    mov word [bp+6], 0x1000             ; Segment 
+    mov dword [bp+8], 2048              ; LBA (Kernel is at 2048)
     mov dword [bp+12], 0x00
     mov si, bp
-    mov dl, [boot_drive] 
+    mov dl, [boot_drive]                ; Get the boot drive, put it in dl
     mov ah, 0x42
-    int 0x13
-    jnc .kernel_ok
-    loop .load_kernel
-    mov si, msg_load_fail
-    jmp error
+    int 0x13                            ; Read from disk
+    jnc .kernel_ok                      ; If read succeeded, jump to kernel_ok
+    loop .load_kernel                   ; Else loop back to .load_kernel
+    mov si, msg_load_fail               ; Couldn't load the kernel, show error and halt CPU
+    jmp error   
 
 .kernel_ok:
-    mov ax, 0
-    mov ds, ax
+    xor ax, ax                          
+    mov ds, ax                          ; Make sure Data Segment is zero
 
-    lgdt [gdt_descriptor]
+    lgdt [gdt_descriptor]               ; Load GDT Descriptor
 
     mov eax, cr0
-    or eax, 1
+    or eax, 1                           ; Set PM bit to 1
     mov cr0, eax
 
-    jmp CODE_SEG:init_pm
+    jmp CODE_SEG:init_pm                ; Do far jump into init_pm
 
-
+; Function to enable to A20 line
 enable_a20:
-    ; Fast method
     in al, 0x92
     test al, 2
     jnz .done
     or al, 2
     out 0x92, al
 .done:
-    ; Verify
     xor ax, ax
     mov es, ax
     mov di, 0x0500
@@ -90,6 +86,7 @@ enable_a20:
     stc
     ret
 
+; Prints message to the screen in Real Mode
 print_string:
     pusha
     mov ah, 0x0E
@@ -103,6 +100,7 @@ print_string:
     popa
     ret
 
+; Error function, something went bad, print a message and halt the CPU
 error:
     call print_string
     mov si, msg_halt
@@ -110,9 +108,11 @@ error:
     cli
     hlt
 
+; 32-bit Protected Mode Initialization
 [BITS 32]
 init_pm:
-%include "build/kernel_sizes.inc"
+%include "build/kernel_sizes.inc"       ; This needs to be included here so 32-bit
+                                        ; protected mode knows about KERNEL_MOVSDS
     mov ax, DATA_SEG
     mov ds, ax
     mov es, ax
@@ -120,36 +120,37 @@ init_pm:
     mov gs, ax
     mov ss, ax
     mov esp, 0x400000
-    mov ebp, esp
+    mov ebp, esp                        ; Set the stack in Protected Mode
     
     mov esi, 0x10000
-    mov edi, 0x100000
-    mov ecx, KERNEL_MOVSDS 
-    rep movsd
+    mov edi, 0x100000       
+    mov ecx, KERNEL_MOVSDS  
+    rep movsd                           ; Move the kernel from 0x10000 to 0x100000 (1MB) 
 
     jmp 0x100000
 
+; Helper function to print 16-bit Hex values
 [BITS 16]
 print_hex16:
     ; Input: AX = value to print
     pusha
-    mov cx, 4           ; 4 hex digits (16 bits)
-    mov dx, ax          ; Backup AX since we modify it
+    mov cx, 4                           ; 4 hex digits (16 bits)
+    mov dx, ax                          ; Backup AX since we modify it
 .next_digit:
-    rol dx, 4           ; Rotate left 4 bits (bring next nibble to low 4 bits)
+    rol dx, 4                           ; Rotate left 4 bits (bring next nibble to low 4 bits)
     mov bx, dx
-    and bx, 0x000F      ; Isolate lowest 4 bits
-    mov al, [hex_chars + bx] ; Get ASCII character
-    mov ah, 0x0E        ; BIOS teletype function
-    int 0x10            ; Print character
+    and bx, 0x000F                      ; Isolate lowest 4 bits
+    mov al, [hex_chars + bx]            ; Get ASCII character
+    mov ah, 0x0E                        ; BIOS teletype function
+    int 0x10                            ; Print character
     loop .next_digit
     popa
     ret
 
-; GDT
+; GDT Table
 gdt_start:
     dd 0x0
-    dd 0x0
+    dd 0x0                              ; First needs to be 0
 
 gdt_code:
     dw 0xFFFF
@@ -157,34 +158,35 @@ gdt_code:
     db 0x0
     db 10011010b
     db 11001111b
-    db 0x0
+    db 0x0                              ; Second is for Code Segment
 
 gdt_data:
     dw 0xFFFF
     dw 0x0
     db 0x0
     db 10010010b
-    db 11001111b
-    db 0x0
+    db 11001111b   
+    db 0x0                              ; Third for Data Segment
 
 gdt_end:
 
 
 gdt_descriptor:
     dw gdt_end - gdt_start - 1
-    dd gdt_start
+    dd gdt_start                        ; Calculated the correct GDT values
 
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
-hex_chars db '0123456789ABCDEF'
+hex_chars db '0123456789ABCDEF'         ; Hex characters
+
 ; Data
 msg_a20_fail    db 'A20 fail',0
 msg_load_fail   db 'Load fail',0
-msg_kernel_invalid db 'Bad kernel',0
 msg_halt        db 'Halted',0
 
 boot_drive      db 0
 
+; Fill in the rest of the file with zeros and add the mandatory boot magic number at the end
 times 510-($-$$) db 0
 dw 0xAA55
